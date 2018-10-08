fb8a2dc7fdce7f22aedcea569dfe3ea42d6904e1
SamHoltz <holtzman.samuel1@gmail.com>
2018-10-08 14:02:13 -0500

    updated file structure, p/nav working

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
-import Wrapper from "./pages/wrapper";
-import Main from "./pages/main";
-import ItemList from "./pages/itemList";
-import SurvivorList from "./components/survivorList";
+// import Wrapper from "./pages/wrapper";
+// import Main from "./pages/main";
+// import ItemList from "./pages/itemList";
+// import SurvivorList from "./components/survivorList";
+import Paragraph from "./components/Paragraph"
+import Image from "./components/Image"
+import Nav from "./components/Nav"
 
 const App = () => (
-    <Wrapper>
-        
-        <Main>
-            <Image/>
-            <ItemList/>
-            <SurvivorList/>
-            <Paragraph/>
-            <Buttons/>
-        </Main>
-        
-    </Wrapper>
+  <div>
+    <Nav>
+      Escape from Chicago
+    </Nav>
+    {/* <Wrapper> */}
+    {/* <Main> */}
+    <Image />
+    {/* <ItemList /> */}
+    {/* <SurvivorList /> */}
+    <Paragraph>
+      hello
+      </Paragraph>
+    {/* <Buttons />  */}
+    {/* </Main> */}
+    {/* </Wrapper>  */}
+  </div>
 );
 
 export default App;
