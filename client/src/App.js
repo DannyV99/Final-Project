2aeeb6f5aed5aff19190d62a7e5973a74b69e64f
Colin <colingoodale@gmail.com>
2018-10-12 14:32:23 -0500

    fixed proxy, db sending data to DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,41 @@
 import React, { Component } from "react";
-import axios from 'axios';
 import ItemList from "./components/ItemList";
 import ListItem from "./components/ItemList/ListItems";
 import Paragraph from "./components/Paragraph";
 import Image from "./components/Image";
 import Nav from "./components/Nav";
 import items from "./items.json";
-import pagesmock from "./pagesmock.json";
+import API from "./utils/API";
 import Buttons from "./components/Buttons";
 
 class App extends Component {
   state = {
     items,
-    pagesmock,
-    currentLocation: pagesmock[0],
+    currentLocation: {},
   }
 
   handleLocationChange = id => {
-    const newLocation = this.state.pagesmock.filter(location => {
-      return id === location.id
-    })
-    console.log(newLocation);
-    this.setState({ currentLocation: newLocation[0] })
+    API.getPage(id)
+      .then(res => {
+        console.log("response", res);
+        this.setState({ currentLocation: res.data[0] })
+        console.log(res.data[0])
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   componentDidMount() {
-    axios.get('api/page')
+    API.getPage(0)
       .then(res => {
-        this.setState({ currentLocation: pagesmock[0] })
-        console.log(this.state.currentLocation);
-      });
+        console.log("response", res);
+        this.setState({ currentLocation: res.data[0] })
+        console.log(res.data[0])
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render() {
@@ -37,12 +43,12 @@ class App extends Component {
       < div >
         <Nav>
           Escape Chicago
-    </Nav>
+        </Nav>
         <Image imgURL={this.state.currentLocation.imageURL} />
         {console.log(this.state.currentLocation)}
         <Paragraph>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </Paragraph>
+          {this.state.currentLocation.paragraph}
+        </Paragraph>
         <ItemList>
           {this.state.items.map(item => (
             <ListItem
@@ -54,21 +60,16 @@ class App extends Component {
             />
           ))}
         </ItemList>
-        {console.log(this.state.currentLocation)}
-        {
-          this.state.currentLocation.options.map(option => (
-            <Buttons
-              key={option[1]}
-              buttonText={option[0]}
-              handleLocationChange={() => this.handleLocationChange(option[1])}
-            />
-          ))
-        })
-      }
-      </div >
+        {this.state.currentLocation.options && this.state.currentLocation.options.map(option =>
+          <Buttons
+            key={option[1]}
+            id={option[1]}
+            buttonText={option[0]}
+            handleLocationChange={this.handleLocationChange}
+          />
+        )};
+      </div>
     )
   }
-
 };
-
 export default App;
--- client/src/components/Buttons/Buttons.js
+++ client/src/components/Buttons/Buttons.js
@@ -4,7 +4,7 @@ const wellStyles = { maxWidth: 400, margin: '0 auto 10px' };
 
 const Buttons = props => (
     <div className="well" style={wellStyles}>
-        <button bsStyle="danger" bsSize="large" onClick={props.handleLocationChange} block>
+        <button bsStyle="danger" bsSize="large" onClick={() => props.handleLocationChange(props.id)} block>
             {props.buttonText}
         </button>
     </div>
